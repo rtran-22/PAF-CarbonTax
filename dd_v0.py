4b5fb8ca9e70e35de9614ee67c050e37e31c774e
Tran-Thanh Raphaël <raphael.tran-thanh@telecom-paris.fr>
2023-06-20 16:06:58 +0000

    ça marche pas

--- dd_v0.py
+++ dd_v0.py
@@ -1,7 +1,6 @@
 import cvxpy as cp
 from functools import partial
 import math
-import pandas as pd
 import matplotlib.pyplot as plt
 from math import log as ln
 
@@ -21,12 +20,14 @@ class agent:
         sum = 0
         for i in range(0, len(self.product_l)):
             sum += product_q[i]*(self.product_l[i].carb) 
-        return sum - u(self.coefs, product_q)
+        return sum - self.u(product_q)
 
     def somme_depenses(self, product_q):
         sum = 0
-        for i in range(len(product_q)):
-            sum += product_q[i]*(self.product_l[i].price)
+        i = 0
+        for p in product_q:
+            sum += p*(self.product_l[i].price)
+            i += 1
         return sum
         
 class commu:
@@ -48,7 +49,7 @@ class commu:
         #on ajoute les contraintes
         for i in range(0, len(self.agent_list)):
             constraints.append(self.agent_list[i].somme_depenses(x[i]) <= self.agent_list[i].budget)
-            constraints.append(x[i][0] + x[i][1] + x[i][2] + x[i][3] + x[i][4] + x[i][5] == 1)
+            constraints.append(x[i][0] + x[i][1] + x[i][2] + x[i][3] == 1)
         
         def f(m):
             return self.fonc_i(lmbd=lmbd, C=C, x_mat=m)
@@ -57,7 +58,7 @@ class commu:
         prob = cp.Problem(objective, constraints)
         prob.solve()
         
-        #print(x.value)
+        print(x.value)
         return f(x.value)
 
         
@@ -67,10 +68,10 @@ class commu:
 #MODELE 
 
 def u(l, x):
-    prod = 1
+    sum = 0
     for i in range(NB_DESTINATIONS):
-        prod *= (x[i]+l[2*i+1])**l[2*i]
-    return ln(prod)
+        sum += l[2*i]*(x[i]+l[2*i+1])**0.5
+    return sum
         
 
 #MODELE NUMERIQUE 
@@ -83,22 +84,26 @@ FRANCE = product(150, 1)
 JAPON = product(1200, 700)
 USA = product(400, 350)
 ALLEMAGNE = product(275, 30)
+SUISSE = ...
 
 prod_l = [FRANCE, JAPON, USA, ALLEMAGNE]
 
-agent1 = agent(partial(u, l1), prod_l)
-agent2 = agent(partial(u, l2), prod_l)
-agent3 = agent(partial(u, l3), prod_l)
+l1 = [1.6, 1, 0.3, 0.1, 0.3, 1.5, 1, 0.8] #Veut rester en France et riche 
+l2 = [0.8, 1, 1.2, 0.1, 1.6, 0.3, 0.8, 0.8] #Se faire de la thune et riche
+l3 = [1, 1, 1, 0.1, 1, 0.3, 1, 0.8] #N'a pas d'argent et osef
+agent1 = agent(partial(u, l1), prod_l, l1, 30000)
+agent2 = agent(partial(u, l2), prod_l, l2, 30000)
+agent3 = agent(partial(u, l3), prod_l, l3, 1500)
 
 ens = commu([agent1, agent2, agent3])
-
+ens.D(10000, 0)
 
 l = [g/10 for g in range(0,100)]
 #for g in l:
 #    print("D(" + str(g/200) +") = "+ str(ens.D(100, g/200)))
 
-plt.plot(l, [ens.D(100, g) for g in l])
+'''plt.plot(l, [ens.D(100, g) for g in l])
 plt.plot(l, [ens.D(75, g) for g in l])
 plt.plot(l, [ens.D(50, g) for g in l])
-
+'''
 plt.show()
